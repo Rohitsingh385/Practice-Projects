450e9623ec1ec18c65c59304c9dbe9c21239c361
unknown <rk301855@gmail.com>
2023-06-10 07:32:13 +0100

    web 1.0 first commit

---
+++ WEB DEVLOPMENT/web 1.0/Readme.txt
@@ -0,0 +1,5 @@
+v 1.0.0 SIMPLE LOGIN FORM
+
+The folder consist of a user form where a user can see and update his data.
+
+technologies: html, css, javaScript
---
+++ WEB DEVLOPMENT/web 1.0/app.js
@@ -0,0 +1,35 @@
+function handleEditProfile(){
+
+    var updateName = document.getElementById("input-name")
+    var value1 = document.getElementById("name").textContent
+    updateName.value = value1
+
+    var updateEmail = document.getElementById("input-email")
+    var value2 = document.getElementById("email").textContent
+    updateEmail.value = value2
+
+    var updateInterest = document.getElementById("input-interest")
+    var value3 = document.getElementById("interest").textContent
+    updateInterest.value = value3
+     
+    document.getElementById("update-container").style.display = "block"
+    document.getElementById("main-container").style.display = "none"
+}
+
+function handleUpdateProfile(){
+
+    var updateName = document.getElementById("input-name").value
+    var value1 = document.getElementById("name")
+    value1.textContent = updateName
+
+    var updateEmail = document.getElementById("input-email").value
+    var value2 = document.getElementById("email")
+    value2.textContent = updateEmail
+
+    var updateInterest = document.getElementById("input-interest").value
+    var value3 = document.getElementById("interest")
+    value3.textContent = updateInterest
+    
+    document.getElementById("update-container").style.display = "none"
+    document.getElementById("main-container").style.display = "block"
+}
